3b2403756faa8d0ce64a5b521512ac6533dcbeee
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-03-15 16:20:38 -0300

    fix: conn is null

--- Postgres.PgVector.Study/Program.cs
+++ Postgres.PgVector.Study/Program.cs
@@ -8,10 +8,10 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        var connectionString = builder.Configuration.GetConnectionString("PgSql");
+        var connectionString = builder.Configuration["PgSql:ConnectionString"];
 
         if (string.IsNullOrEmpty(connectionString))
-            throw new Exception("ConnectionStrings:PgSql Is Null");
+            throw new Exception("PgSql:ConnectionString Is Null");
 
         // Add services to the container.
         builder.Services.AddDbContext<DatabaseContext>(options =>
